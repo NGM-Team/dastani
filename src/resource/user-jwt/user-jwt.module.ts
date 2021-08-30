8f589d072a93db46a90f03016fe0771c1d06f837
Tefan Haetami <tefanhaetami87@gmail.com>
2021-08-30 17:05:01 +0700

    import authmodule in user-jwt module

--- src/resource/user-jwt/user-jwt.module.ts
+++ src/resource/user-jwt/user-jwt.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserJwtService } from './user-jwt.service';
 import { UserJwtController } from './user-jwt.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UserJwtController],
   providers: [UserJwtService],
+  imports: [AuthModule],
 })
 export class UserJwtModule {}
